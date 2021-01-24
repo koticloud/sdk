6bd20e1d9a49a27ea9cd5335717fdd3c86fc753c
Alexander Zavyalov <whmordred@gmail.com>
2021-01-24 13:40:46 +0800

    Svelte - renamed Page.svelte -> PageRenderer.svelte

--- svelte-components.js
+++ svelte-components.js
@@ -1,9 +1,9 @@
 // Navigator (router)
-import Page from './modules/ui-components/svelte/navigator/Page.svelte';
+import PageRenderer from './modules/ui-components/svelte/navigator/PageRenderer.svelte';
 import PageLink from './modules/ui-components/svelte/navigator/PageLink.svelte';
 
 export {
     // Navigator (router)
-    Page,
+    PageRenderer,
     PageLink,
 };
