670a2e1d4e203e495cfba1955e972cdb26d817c0
Alexander Zavyalov <whmordred@gmail.com>
2021-03-02 19:38:25 +0800

    Panel component - added slots

--- modules/ui-components/svelte/Panel.svelte
+++ modules/ui-components/svelte/Panel.svelte
@@ -8,13 +8,38 @@
     color: var(--kc-color--text-main);
     min-height: 2.5rem;
 
-    display: flex;
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
     align-items: center;
-    justify-content: center;
-    padding: 0 1rem;
+
+    .left, .right {
+        white-space: nowrap;
+    }
+
+    .middle {
+        text-align: center;
+        margin: 0 1rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
+    .right {
+        text-align: right;
+    }
 }
 </style>
 
 <div class="kc-component--panel" class:kc-component--default-style={defaultStyle}>
-    <slot></slot>
+    <div class="left">
+        <slot name="left"></slot>
+    </div>
+
+    <div class="middle">
+        <slot></slot>
+    </div>
+
+    <div class="right">
+        <slot name="right"></slot>
+    </div>
 </div>
