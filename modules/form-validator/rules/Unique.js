e7ef2fb0e79dcc908dc237d2ad48b9188c42de7f
Alexander Zavyalov <whmordred@gmail.com>
2021-06-02 14:17:55 +0800

    Unique validation - ability to exclude a row by ID

--- modules/form-validator/rules/Unique.js
+++ modules/form-validator/rules/Unique.js
@@ -4,9 +4,15 @@ import Rule from './Rule.js';
 class Unique extends Rule
 {
     async isValid(value) {
-        let exists = await App.get().db.collection(this.args[0])
-            .where(this.args[1], value)
-            .first();
+        let query = App.get().db.collection(this.args[0])
+            .where(this.args[1], value);
+        
+        // Optionally exclude the row with the specified _id
+        if (this.args[2] !== undefined && this.args[2] !== null) {
+            query = query.where('_id', '!=', this.args[2]);
+        }
+
+        let exists = await query.first();
 
         return !exists;
     }
